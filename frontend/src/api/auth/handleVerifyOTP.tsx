06cc78abe168a18374d3443a73dc6fa72f1060aa
minhhuy <161584434+caominhhuy204@users.noreply.github.com>
2026-04-09 01:36:17 +0700

    Reapply "token"
    This reverts commit 87a449cc0a708b53dc1eae41553cd608f82caca3.

--- frontend/src/api/auth/handleVerifyOTP.tsx
+++ frontend/src/api/auth/handleVerifyOTP.tsx
@@ -3,7 +3,6 @@ import api from "@/utils/axiosClient";
 const handleVerifyOTP = async (otp: string, token:string) => {
   try {
     const res = await api.post("/api/auth/verify", { otp, token});
-    console.log(res.data);
     return res.data;
   } catch (error) {
     console.error(error);
